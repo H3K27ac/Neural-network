594b3631fa9a374e2dac9a0b05a63f229039254f
H3K27ac <101502649+H3K27ac@users.noreply.github.com>
2024-11-12 17:44:54 +0800

    Defined classes to construct network

--- calculation.js
+++ calculation.js
@@ -22,18 +22,30 @@ class DenseLayer extends Layer {
     forward(neurons) {
         this.neurons = neurons;
         let layerOutput = this.matrixVectorProduct(this.weights,neurons);
+        layerOutput = this.vectorVectorSum(layerOutput,this.biases);
+        return layerOutput;
     }
+
+    backward() {}
 
     matrixVectorProduct(matrix,vector) {
         let outputVector = [];
-        for (let i = 0; i < matrix.length; i++) {
+        for (let i = 0; i < this.outputSize; i++) {
             let sum = 0;
-            for (let j = 0; j < matrix[i].length; j++) {
+            for (let j = 0; j < this.inputSize; j++) {
                 sum += matrix[i*this.outputSize+j] * vector[j];
             }
             outputVector.push(sum);
         }
         return outputVector;
+    }
+
+    vectorVectorSum(vector1,vector2) {
+        if (vector1.length !== vector2.length); // Error
+        for (let i = 0; i < vector1.length; i++) {
+            vector1[i] += vector2[i];
+        }
+        return vector1;
     }
 }
 
